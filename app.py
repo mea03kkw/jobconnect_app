cab3aecb1aa8eeddb1f4f9f42cc409aae678f678
mea03kkw <kaye1209@gmail.com>
2026-01-06 14:41:35 +0800

    Remove comment

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Modified to remove mail
 from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
